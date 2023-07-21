6f242fd680eb45ff22dab4d29e293a00b2f4434d
dojo2023-wada <wadakakoplusdojo2023@seplus2016.onmicrosoft.com>
2023-07-21 16:23:42 +0900

    entity_和田担当分

--- server/feseek/src/main/java/com/feseek/entity/InquiryContent.java
+++ server/feseek/src/main/java/com/feseek/entity/InquiryContent.java
@@ -1,5 +1,28 @@
 package com.feseek.entity;
 
-public class InquiryContent {
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="inquiry_contents")
+public class  InquiryContent{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer inquiries_id;
+	private String users_id;
+	private Date inq_date;
+	private String inquiry;
 
 }
--- server/feseek/src/main/java/com/feseek/entity/Review.java
+++ server/feseek/src/main/java/com/feseek/entity/Review.java
@@ -1,5 +1,36 @@
 package com.feseek.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="reviews")
 public class Review {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String users_id;
+	private Integer events_id;
+	private Integer satisfaction;
+	private Integer security;
+	private Integer again;
+	private Integer atmosphere;
+	private Integer continuation;
+	private Date rev_date;
+	private String rev_title;
+	private String comment;
+	private String rev_image;
+	
 
 }
