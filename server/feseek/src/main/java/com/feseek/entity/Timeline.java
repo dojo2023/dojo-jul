831a1b09e49398feed1ac63aa740c7903b638fb4
dojo2023-urabe <130334554+dojo2023-urabe@users.noreply.github.com>
2023-07-21 16:30:48 +0900

    entity timeline　占部

--- server/feseek/src/main/java/com/feseek/entity/Timeline.java
+++ server/feseek/src/main/java/com/feseek/entity/Timeline.java
@@ -1,5 +1,26 @@
 package com.feseek.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="timelines")
 public class Timeline {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Integer event_id;
+	
+	private String infomation;
 
 }
