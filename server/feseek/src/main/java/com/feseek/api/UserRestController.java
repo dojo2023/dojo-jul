aa98db70a58be184f6a50c4a486dd24800f496ee
dojo2023-haruta <130334521+dojo2023-haruta@users.noreply.github.com>
2023-07-24 15:48:42 +0900

    新規登録、編集、削除機能

--- server/feseek/src/main/java/com/feseek/api/UserRestController.java
+++ server/feseek/src/main/java/com/feseek/api/UserRestController.java
@@ -1,32 +1,79 @@
 package com.feseek.api;
 
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.feseek.entity.User;
+import com.feseek.repository.UsersRepository;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
-//    private UserRepository userRepository;
+    private UsersRepository usersRepository;
 
-  //  @Autowired
-   // public UserRestController(UserRepository userRepository) {
-    //    this.userRepository = userRepository;
-    //}
-
-   // @PostMapping("/register")
-   // public String registerUser(@RequestBody UserRegistrationRequest request) {
+    public UserRestController(UsersRepository userRepository) {
+        this.usersRepository = userRepository;
+    }
+    //ユーザーの新規登録
+    @PostMapping("/register")
+    //ここのUserが違うかも。
+    public String registerUser(@RequestBody User request) {
         // ユーザー情報のバリデーションを行います（省略）。
         
-        // UserRegistrationRequestからUserエンティティを作成します。
-     //   User user = new User();
-      //  user.setUsername(request.getUsername());
-       // user.setEmail(request.getEmail());
-        //user.setPassword(request.getPassword());
-       // user.setUserType(request.getUserType());
+      //   UserRegistrationRequestからUserエンティティを作成します。
+        User user = new User();
+        user.setUser_name(request.getUser_name());
+        user.setE_mail(request.getE_mail());
+        user.setPassword(request.getPassword());
+        user.setCategories_id(request.getCategories_id());
 
-        // データベースにユーザーを保存します。
-       // userRepository.save(user);
+    //     データベースにユーザーを保存します。
+        usersRepository.save(user);
 
-      //  return "ユーザーが正常に登録されました。";
+        return "ユーザーが正常に登録されました。";
+    }
+    
+ // ユーザーの削除
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable String id) {
+        // 指定されたIDのユーザーをデータベースから取得します。
+        Optional<User> optionalUser = usersRepository.findById(id);
+
+        // ユーザーが存在するか確認します。
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            usersRepository.delete(user);
+            return "ユーザーが正常に削除されました。";
+        } else {
+            return "指定されたIDのユーザーは見つかりませんでした。";
+        }
+    }
+    
+    // ユーザー情報の更新
+    @PutMapping("/users/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody User updatedUser) {
+        // 指定されたIDのユーザーをデータベースから取得します。
+        Optional<User> optionalUser = usersRepository.findById(id);
+
+        // ユーザーが存在するか確認します。
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            // パスワード、メールアドレス、ユーザー名を更新します。
+            user.setPassword(updatedUser.getPassword());
+            user.setE_mail(updatedUser.getE_mail());
+            user.setUser_name(updatedUser.getUser_name());
+            usersRepository.save(user);
+            return "ユーザー情報が正常に更新されました。";
+        } else {
+            return "指定されたIDのユーザーは見つかりませんでした。";
+        }
+    }
 }
