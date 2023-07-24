964bf4f6bf8f697e23d2151227d064efe33a066a
dojo2023-urabe <130334554+dojo2023-urabe@users.noreply.github.com>
2023-07-24 17:12:07 +0900

    イベント投稿（主催者）・タイムライン

--- server/feseek/src/main/java/com/feseek/api/EventPostRestController.java
+++ server/feseek/src/main/java/com/feseek/api/EventPostRestController.java
@@ -1,6 +1,34 @@
 package com.feseek.api;
 
-public class EventPostRestController {
-	//イベント投稿(主催者側)
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.feseek.entity.Event;
+import com.feseek.repository.EventsRepository;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventPostRestController {
+
+    @Autowired
+    private EventsRepository eventsRepository;
+
+    // イベント投稿(主催者側)
+    @PostMapping("/add")
+    protected Event add(@RequestBody Event event, @RequestParam String users_id) {
+        // ユーザーIDを投稿にセットして紐づける
+        event.setUsers_id(users_id);
+
+        // 投稿をデータベースに保存
+        eventsRepository.save(event);
+
+        return event;
+    }
 }
+
+
+
