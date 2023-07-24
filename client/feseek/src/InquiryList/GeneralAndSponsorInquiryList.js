20fc71ce8c76780493a678a7b9b39e42afd048dd
dojo2023-ishido-r <IshidoRikaplusdojo2023@seplus2016.onmicrosoft.com>
2023-07-24 17:17:32 +0900

    担当ページ画面遷移、モーダル表示

--- client/feseek/src/InquiryList/GeneralAndSponsorInquiryList.js
+++ client/feseek/src/InquiryList/GeneralAndSponsorInquiryList.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import './InquiryList.css';
 
 export default class GeneralAndSponsorInquiryList extends React.Component{
 
+    //親コンポーネントから受け取るデータなどがpropsに入っている。
+    constructor(props) {
+        super(props);
+        //stateの設定。
+        this.state = {
+            // books:[]
+            subject: "",
+            inquiry: "",
+            showModal: false,
+            modSubject: "",
+            modInquiry: "",
+        }
+    }
+    
+    //モーダルウィンドウの表示切り替え
+    toggleModal = () => {
+        const{showModal} = this.state;
+        this.setState({
+            showModal: !showModal
+        });
+    }
+
+    //お問い合わせ送信ボタンを押下したときに実行する
+    modDetail = () => {
+        this.toggleModal();
+    }
+
+
     render(){
+
+            const {showModal} =this.state;
+
         return(
             <div>
                 <div>
@@ -10,14 +42,16 @@ export default class GeneralAndSponsorInquiryList extends React.Component{
                 </div>
 
 
-                <button type="submit">お問い合わせ送信</button>
+                <button onClick={() =>{this.modDetail()}} >お問い合わせ送信</button>
 
                 <table>
                     <tr>
                         <td>日時</td>
                         <td>件名</td>
                         <td>内容</td>
-                        <td><button type="submit" name="register">確認</button></td>
+                        <td>
+                            <button name="register">確認</button>
+                        </td>
                     </tr>
                 </table>
 
@@ -28,6 +62,21 @@ export default class GeneralAndSponsorInquiryList extends React.Component{
                 <div>
                     <a href="./EventList">戻る</a>
                 </div>
+
+                {/* モーダルウィンドウ(送信) */}
+                {showModal &&
+                    <div id="overlay">
+                        <div id="content">
+
+                                件名<input type="text" name="modSubject"/>
+                                内容<input type="text" name="modInquiry"/>
+
+                            <button onClick={this.toggleModal}>閉じる</button>
+                            <button type="submit" name="post">送信</button>
+                        </div>
+                    </div>
+                }
+
             </div>
         );
     }
