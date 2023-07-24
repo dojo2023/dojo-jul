ded8716931a458dea4e7765bb1b17ebb0e4a6489
dojo2023-omori <130334772+dojo2023-omori@users.noreply.github.com>
2023-07-24 11:29:39 +0900

    イベント、ジャンルリポジトリ

--- server/feseek/src/main/java/com/feseek/api/EventSearchRestController.java
+++ server/feseek/src/main/java/com/feseek/api/EventSearchRestController.java
@@ -1,20 +1,35 @@
 package com.feseek.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.feseek.entity.Event;
-import com.feseek.repository.EventsRepository;
-
 public class EventSearchRestController 
 {
-	
+	/*
 	@Autowired
 	private EventsRepository repository;
-	
-	//イベント検索
-	@GetMapping("/api/events/search")
-	protected List<Events> search(@RequestBody Event event)
-	
+	//イベント検索(キーワード)
+	@GetMapping("/api/event/search")
+	protected List<Event> search(@RequestBody Event event)
+	{
+		String keyword = event.getKeyword();
+		
+		return repository.//ここに検索の関数名
+				 ("%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				 ,"%" + keyword + "%"
+				);
+	}
+	*/
 }
