c03bf38371811a8921d7eb57f382d639dbcf7a92
dojo2023-urabe <130334554+dojo2023-urabe@users.noreply.github.com>
2023-07-24 14:19:55 +0900

    ログイン・ログアウトたぶん完成　ユーザーリポジトリ

--- server/feseek/src/main/java/com/feseek/api/LoginRestController.java
+++ server/feseek/src/main/java/com/feseek/api/LoginRestController.java
@@ -1,42 +1,59 @@
-//package com.feseek.api;
-//
-//import java.util.Map;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.feseek.entity.User;
-//import com.feseek.repository.UsersRepository;
-//
-//	//ログイン
-//	@RestController
-//	@RequestMapping("/api")
-//	public class LoginRestController {
-//
-//	    @Autowired
-//	    private UsersRepository usersRepository;
-//
-//	    @PostMapping("/login")
-//	    public ResponseEntity<String> login(@RequestBody Map<String, String> loginRequest) {
-//	        String id = loginRequest.get("id");
-//	        String password = loginRequest.get("password");
-//
-////	       // User user = usersRepository.findById(id);
-////
-////	        //if (user != null && user.getPassword().equals(password)) {
-////	            // 認証成功
-////	            return new ResponseEntity<>("Login successful", HttpStatus.OK);
-////	        } else {
-////	            // 認証失敗
-////	            return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED);
-////	        }
-//	    }
-//	}
-	
-	//ログアウト
+package com.feseek.api;
 
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.feseek.entity.User;
+import com.feseek.repository.UsersRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+	//ログイン・ログアウト
+	@RestController
+	@RequestMapping("/api")
+	public class LoginRestController {
+
+	    @Autowired
+	    private UsersRepository usersRepository;
+
+	    //ログイン
+	    @PostMapping("/login")
+	    public ResponseEntity<String> login(@RequestBody Map<String, String> loginRequest, HttpServletRequest request) {
+	        String id = loginRequest.get("id");
+	        String password = loginRequest.get("password");
+
+	        Optional<User> optionalUser = usersRepository.findById(id);
+	        User user = optionalUser.orElse(null); // デフォルト値としてnullを設定
+
+	        if (user != null && user.getPassword().equals(password)) {
+	            // 認証成功
+	            HttpSession session = request.getSession();
+	            session.setAttribute("id", id);
+	            return new ResponseEntity<>("Login successful", HttpStatus.OK);
+	        } else {
+	            // 認証失敗
+	            return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED);
+	        }
+	    }
+
+	    //ログアウト
+	    @PostMapping("/logout")
+	    public ResponseEntity<String> logout(HttpServletRequest request) {
+	        HttpSession session = request.getSession(false);
+	        if (session != null) {
+	            session.removeAttribute("id");
+	            session.invalidate(); // セッションを無効化
+	        }
+	        return new ResponseEntity<>("Logout successful", HttpStatus.OK);
+	    }
+
+}
--- server/feseek/src/main/java/com/feseek/repository/UsersRepository.java
+++ server/feseek/src/main/java/com/feseek/repository/UsersRepository.java
@@ -1,10 +1,22 @@
 package com.feseek.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feseek.entity.User;
 
 public interface UsersRepository extends JpaRepository<User, String> {
+	// ユーザIDを使用してユーザー情報を取得するためのメソッド
+    Optional<User> findById(String id);
 
+    // ユーザー名とパスワードを使用してユーザー情報を取得するためのメソッド
+    Optional<User> findByIdAndPassword(String id, String password);
+    
+    // メールアドレスを使用してユーザー情報を取得するためのメソッド
+    Optional<User> findByE_mail(String e_mail);
+
+    // ユーザー名またはメールアドレスを使用してユーザー情報を取得するためのメソッド
+    Optional<User> findByUser_nameOrE_mail(String user_name, String e_mail);
 }
