bb68de8c0c724513deff2eee0f6118f999c9fb37
dojo2023-urabe <130334554+dojo2023-urabe@users.noreply.github.com>
2023-07-25 18:05:21 +0900

    Update TimelinesRepository.java

--- server/feseek/src/main/java/com/feseek/repository/TimelinesRepository.java
+++ server/feseek/src/main/java/com/feseek/repository/TimelinesRepository.java
@@ -1,11 +1,13 @@
 package com.feseek.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feseek.entity.Timeline;
 
 public interface TimelinesRepository extends JpaRepository<Timeline, Integer> {
 	 // イベントIDに対応するタイムライン情報を取得するメソッド
-    //List<Timeline> findByEventId(Integer event_id);
+    List<Timeline> findByEventId(Integer events_id);
 
 }
