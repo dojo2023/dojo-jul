38741ad491d3de5a64bb6f9428e85cc42afb5e77
dojo2023-wada <wadakakoplusdojo2023@seplus2016.onmicrosoft.com>
2023-07-25 14:20:22 +0900

    repository　コメントアウト

--- server/feseek/src/main/java/com/feseek/repository/ReviewsRepository.java
+++ server/feseek/src/main/java/com/feseek/repository/ReviewsRepository.java
@@ -1,7 +1,5 @@
 package com.feseek.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feseek.entity.Review;
@@ -10,13 +8,13 @@ public interface ReviewsRepository extends JpaRepository<Review, Integer> {
 
 	//イベント詳細ページ内口コミ一覧(イベントIDで完全一致検索)
 	//List<Review> findEventsReview(Integer events_id);
-	List<Review> findByEvents_id(Integer events_id);
+	//List<Review> findByEvents_id(Integer events_id);
 	
 	//並び替えあり
 //	List<Review> findEventsReview(Integer events_id, Sort sort);
 	
 	//自分が投稿した口コミ一覧(ユーザIDで完全一致検索)
-	List<Review> findByUserID(Integer users_id);
+	//List<Review> findByUserID(Integer users_id);
 	
 	//並び替えあり
 //	List<Review> findMyReview(Integer users_id, Sort sort);
