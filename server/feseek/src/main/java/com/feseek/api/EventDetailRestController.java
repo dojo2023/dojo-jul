a682230cfc365882978fc323b9f9323a2c4e7bd6
dojo2023-wada <wadakakoplusdojo2023@seplus2016.onmicrosoft.com>
2023-07-25 15:22:42 +0900

    Update EventDetailRestController.java

--- server/feseek/src/main/java/com/feseek/api/EventDetailRestController.java
+++ server/feseek/src/main/java/com/feseek/api/EventDetailRestController.java
@@ -1,9 +1,10 @@
 package com.feseek.api;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.feseek.entity.Event;
@@ -11,6 +12,7 @@ import com.feseek.repository.EventsRepository;
 
 @RestController
 public class EventDetailRestController {
+	Integer id = 1;
 	
 	//イベント詳細画面
 	
@@ -18,8 +20,17 @@ public class EventDetailRestController {
 	private EventsRepository repository;
 	
 	@GetMapping("/api/event/detail")
-	protected List<Event> findAll(){
-		return repository.findAll();
+	protected Optional<Event> findById(@RequestBody Event event){
+		//return repository.findById(id);
+		return repository.findById(event.getId());
 	}
+	
+	/*
+	@GetMapping("/api/event/detail")
+	protected List<Event> findAll(){
+		return repository.findAll(1);
+	}
+	*/
+	
 	
 }
