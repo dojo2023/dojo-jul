42e300f17a7a5aac87b4710d2c7637730d5fc854
dojo2023-wada <wadakakoplusdojo2023@seplus2016.onmicrosoft.com>
2023-07-25 14:03:00 +0900

    repository コメントアウト

--- server/feseek/src/main/java/com/feseek/repository/TimelinesRepository.java
+++ server/feseek/src/main/java/com/feseek/repository/TimelinesRepository.java
@@ -1,13 +1,11 @@
 package com.feseek.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feseek.entity.Timeline;
 
 public interface TimelinesRepository extends JpaRepository<Timeline, Integer> {
 	 // イベントIDに対応するタイムライン情報を取得するメソッド
-    List<Timeline> findByEvents_id(Integer events_id);
+    //List<Timeline> findByEventId(Integer event_id);
 
 }
--- server/feseek/src/main/java/com/feseek/repository/UsersRepository.java
+++ server/feseek/src/main/java/com/feseek/repository/UsersRepository.java
@@ -1,25 +1,23 @@
 package com.feseek.repository;
 
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feseek.entity.User;
 
 public interface UsersRepository extends JpaRepository<User, String> {
 	// ユーザIDを使用してユーザー情報を取得するためのメソッド
-    Optional<User> findById(String id);
+    //Optional<User> findById(String id);
 
     // ユーザー名とパスワードを使用してユーザー情報を取得するためのメソッド
-    Optional<User> findByIdAndPassword(String id, String password);
+    //Optional<User> findByIdAndPassword(String id, String password);
     
     // メールアドレスを使用してユーザー情報を取得するためのメソッド
-    Optional<User> findByE_mail(String e_mail);
+    //List<User> findByE_mail(String e_mail);
 
     // ユーザー名またはメールアドレスを使用してユーザー情報を取得するためのメソッド
-    Optional<User> findByUser_nameOrE_mail(String user_name, String e_mail);
+    //Optional<User> findByUser_nameOrE_mail(String user_name, String e_mail);
     
     //種別IDを使用してユーザー種別を取得するためのメソッド（いらなければ削除してよし）
-    Optional<User> findByCategories_id(Integer Categories_id);
+    //Optional<User> findByCategories_id(Integer Categories_id);
 }
