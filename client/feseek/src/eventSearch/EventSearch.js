0a03e0572cfcc1f748071833801c05ec12124027
dojo2023-ishido-r <IshidoRikaplusdojo2023@seplus2016.onmicrosoft.com>
2023-07-25 09:31:10 +0900

    担当ページ更新&axiosインストール

--- client/feseek/src/eventSearch/EventSearch.js
+++ client/feseek/src/eventSearch/EventSearch.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class EventSearch extends React.Component{
 
@@ -8,24 +9,22 @@ export default class EventSearch extends React.Component{
                 <h1>イベント検索ページ</h1>
 
                 {/* キーワード検索*/}
-                <p>キーワード検索</p>
                 <div class="keywordSearch">
-                    <input type="text"></input>
+                    <input type="text" placeholder="キーワード検索"></input>
                     <button>検索</button>
                 </div>
+                <br/>
 
                 {/* 詳細検索*/}
-                <p>詳細検索</p>
                 <div class="detailSearch">
                     ジャンル
                     <select name="genre">
-                        <option>ジャンル</option>
-                        <option>b</option>
-                        <option>c</option>
-                        <option>d</option>
+                        <option>食べる</option>
+                        <option>見る</option>
+                        <option>体験する</option>
+                        <option>その他</option>
                     </select>
                     <br/>
-                    
                     
                     日付
                     <input type="date"></input>
@@ -39,14 +38,15 @@ export default class EventSearch extends React.Component{
                         <option>d</option>
                     </select>
                     <br/>
-
                         
                     季節
-                    <input type="radio" name="season" value="1" checked/>a
-                    <input type="radio" name="season" value="2"/>a
-                    <input type="radio" name="season" value="3"/>a
-                    <input type="radio" name="season" value="4"/>a 
+                    <input type="radio" name="season" value="1" checked/>春
+                    <input type="radio" name="season" value="2"/>夏
+                    <input type="radio" name="season" value="3"/>秋
+                    <input type="radio" name="season" value="4"/>冬
+                    <br/>
                         
+                    <button>検索</button>
 
                     <div>
                         <a href="./EventList">戻る</a>
