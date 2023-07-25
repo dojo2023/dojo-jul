92b0078504fad1910f4813ad9b85b1d26e720e0b
dojo2023-KojimaS <130334683+dojo2023-KojimaS@users.noreply.github.com>
2023-07-25 17:03:24 +0900

    pm 17:00

--- client/feseek/src/login/Login.js
+++ client/feseek/src/login/Login.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
+
 import './Login.css';
 
 export default class Login extends React.Component{
@@ -14,26 +16,54 @@ export default class Login extends React.Component{
     onInput = (e) => {
         const {u_id,password}=this.state;
         this.setState ({
-            id:e.target.value
+            id:e.target.value,
+            password:e.target.value,
         });
     }
 
+    onSubmit = () => {
+        //IDとパスワードをapiに渡してチェックしてもらう。
+
+        //OKなら、次の画面（利用者、主催者、管理者）
+        //ここに、ログイン成功した際のurlを記述するのはカッコ悪いので、WebAPIでログイン成功したときのurlを教えて貰う方がすっきり
+        window.location = "/eventSearch";
+        //NGならエラーメッセージ
+    }
+    
+    /*
+    onSubmit = () => {
+        console.log(data);
+        if (data.u_id === "u_id" && data.password === "password"){
+            loginSuccess();
+        }else{
+            loginError();
+        }
+    };
+    loginSuccess = () => {
+        navigate("/");
+    }
+    loginError = () => {
+        false();
+    }
+    */
+
     render(){
+        const ErrorMsg = "";
+
         return(
             <div>
-            <form method="POST" action="">
                 <div>ログインページ</div>
                 {/* <p class="user_p">ID</p> */}
                 <input type="text" class="l_text" name="u_id" minlength="5" required placeholder="ID"></input><br></br>
                 {/* <p class="user_p">Password</p> */}
                 <input type="password" class="l_text" name="u_password" minlength="8" required placeholder="Password"></input><br></br>
-                <p class="error"></p><br></br>
+                <p class="error">{ErrorMsg}</p><br></br>
 
-                <input type="submit" class="l_button" value="ログイン" onclick=""></input><br></br>
+                <input type="button" class="l_button" value="ログイン" onClick={this.onSubmit}></input><br></br>
+
                 <a href="/ConfirmForId">IDを忘れた方はこちら</a><br></br>
                 <a href="/ConfirmForPassword">パスワードを忘れた方はこちら</a><br></br>
                 <a href="/NewUserRegister">新規登録はこちら</a>
-            </form>
             </div>
         )
     }
