c9d1289f4770506054e1a8ae95fd416c332aeb1f
dojo2023-KojimaS <130334683+dojo2023-KojimaS@users.noreply.github.com>
2023-07-26 17:05:40 +0900

    pm 17:00

--- client/feseek/src/user/UserEdit.js
+++ client/feseek/src/user/UserEdit.js
@@ -8,8 +8,8 @@ export default class User extends React.Component{
         super(props);
         this.state = {
             User:[],
-            u_password:"",
-            u_name:"",
+            password:"",
+            name:"",
             mail_ad:"",
             modPassword:"",
             modName:"",
@@ -17,30 +17,40 @@ export default class User extends React.Component{
         }
     }
 
+    //画面で何か入力された時に、その値をstateとして保持する。
+    //これにより、JavaScript動作時に毎回画面を見に行くのではなく、画面と連動したstateだけを見ればよくなる。
+    onInput = (e) => {
+        const name = e.target.name;
+        this.setState({
+            [name]: e.target.value
+        });
+    }  
+
     // 更新ボタン処理
     saveUser = (info) => {
         //postデータ作成
-        const {u_password,u_name,mail_ad} = this.state;
-        const User = {u_password,u_name,mail_ad};
-        axios.post("/",User)
+        const {password,name,mail_ad} = this.state;
+        const User = {password,name,mail_ad};
+        axios.post("/api/user/profile/edit" , User)
         this.setState({
             //stateにデータをいれる
-            modPassword:User[info].u_password,
-            modName:User[info].u_name,
+            modPassword:User[info].password,
+            modName:User[info].name,
             modMail:User[info].mail_ad,
         });
     }
 
     render(){
+        const {password , name , mail_ad} = this.state;
         return(
             <div>
                 <div>ユーザー情報編集ページ</div>
                 {/* <p class="user_p">Password</p> */}
-                <input type="password" class="u_text" name="u_password" minlength="8" required placeholder="Password(8~20文字で入力ください)"></input><br></br>
+                <input type="password" class="u_text" name="u_password" minlength="8" required placeholder="Password(8~20文字で入力ください)" value={password}></input><br></br>
                 {/* <p class="user_p">ユーザー名</p> */}
-                <input type="text" class="u_text" name="u_name" minlength="2" required placeholder="ユーザー名(2~20文字で入力ください)"></input><br></br>
+                <input type="text" class="u_text" name="u_name" minlength="2" required placeholder="ユーザー名(2~20文字で入力ください)" value={name}></input><br></br>
                 {/* <p class="user_p">メールアドレス</p> */}
-                <input type="text" class="u_text" name="mail_ad" required placeholder="メールアドレス"></input><br></br>
+                <input type="text" class="u_text" name="mail_ad" required placeholder="メールアドレス" value={mail_ad}></input><br></br>
                 <p class="error"></p><br></br>
                 <input type="button" class="u_button" value="更新" onclick={this.saveUser}></input>
             </div>
