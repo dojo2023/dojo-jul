b89a05b0c0fdaf078e4bf7c9d369800a27a93057
dojo2023-wada <wadakakoplusdojo2023@seplus2016.onmicrosoft.com>
2023-07-26 14:59:05 +0900

    Update ProfileRestController.java

--- server/feseek/src/main/java/com/feseek/api/ProfileRestController.java
+++ server/feseek/src/main/java/com/feseek/api/ProfileRestController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.feseek.entity.User;
@@ -23,7 +25,18 @@ public class ProfileRestController {
 	}
 	
 	//更新
+	@PostMapping("/api/user/profile/edit")
+	protected User edit(@RequestBody User user) {
+		//対象のIDが存在するかチェックする
+		repository.save(user);
+		return user;
+	}
 	
 	//退会
+	@PostMapping("/user/profile/delete")
+	protected User delete(@RequestBody User user) {
+		repository.delete(user);
+		return user;
+	}	
 
 }
