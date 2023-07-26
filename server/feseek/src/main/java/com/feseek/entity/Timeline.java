070d79d52e6ac3bbb647ccee0ff7a8fead2cfe4e
dojo2023-Hoshino <130334723+dojo2023-Hoshino@users.noreply.github.com>
2023-07-26 09:44:51 +0900

    Timelineエンティティアノテーション追加

--- server/feseek/src/main/java/com/feseek/entity/Timeline.java
+++ server/feseek/src/main/java/com/feseek/entity/Timeline.java
@@ -1,6 +1,7 @@
 package com.feseek.entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,8 +21,10 @@ public class Timeline {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name="events_id")
 	@NonNull
-	private Integer events_id;
+	private Integer eventsId;
 	@NonNull
 	private String infomation;
 
