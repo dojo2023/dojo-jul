20648affb177f89aee641d78dcc033d2f364d6e3
dojo2023-urabe <130334554+dojo2023-urabe@users.noreply.github.com>
2023-07-27 10:01:03 +0900

    コメントアウト外しました

--- server/feseek/src/main/java/com/feseek/api/EventPostRestController.java
+++ server/feseek/src/main/java/com/feseek/api/EventPostRestController.java
@@ -1,13 +1,20 @@
 package com.feseek.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.feseek.entity.Event;
+import com.feseek.repository.EventsRepository;
 
 @RestController
 @RequestMapping("/api/events")
 public class EventPostRestController {
 	
-	/*
+	
 
     @Autowired
     private EventsRepository eventsRepository;
@@ -16,14 +23,14 @@ public class EventPostRestController {
     @PostMapping("/add")
     protected Event add(@RequestBody Event event, @RequestParam String users_id) {
         // ユーザーIDを投稿にセットして紐づける
-        event.setUsers_id(users_id);
+        event.setUsersId(users_id);
 
         // 投稿をデータベースに保存
         eventsRepository.save(event);
 
         return event;
     }
-    */
+    
 }
 
 
