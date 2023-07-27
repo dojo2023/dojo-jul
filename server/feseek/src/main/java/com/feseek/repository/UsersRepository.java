fec5b70a275d871f55ea43f16380673a765cfc1f
dojo2023-urabe <130334554+dojo2023-urabe@users.noreply.github.com>
2023-07-27 15:27:27 +0900

    口コミ完成

--- server/feseek/src/main/java/com/feseek/repository/UsersRepository.java
+++ server/feseek/src/main/java/com/feseek/repository/UsersRepository.java
@@ -1,9 +1,6 @@
 package com.feseek.repository;
 
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feseek.entity.User;
@@ -11,13 +8,13 @@ import com.feseek.entity.User;
 public interface UsersRepository extends JpaRepository<User, String> {
 
     // ユーザー名とパスワードを使用してユーザー情報を取得するためのメソッド
-   Optional<User> findByIdAndPassword(String id, String password);
-    
-    // メールアドレスを使用してユーザー情報を取得するためのメソッド
-   List<User> findByMail(String mail);
-
-    // ユーザー名またはメールアドレスを使用してユーザー情報を取得するためのメソッド
-   List<User> findByUserNameOrMail(String userName, String mail);
+//   Optional<User> findByIdAndPassword(String id, String password);
+//    
+//    // メールアドレスを使用してユーザー情報を取得するためのメソッド
+//   List<User> findByMail(String mail);
+//
+//    // ユーザー名またはメールアドレスを使用してユーザー情報を取得するためのメソッド
+//   List<User> findByUserNameOrMail(String userName, String mail);
     
     //種別IDを使用してユーザー種別を取得するためのメソッド（いらなければ削除してよし）
 //    Optional<User> findByCategoriesId(Integer categoriesId);
