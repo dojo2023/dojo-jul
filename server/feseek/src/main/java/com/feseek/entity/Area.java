98b67d1faceb1b507aec7a2a071d858798a2e150
dojo2023-Hoshino <130334723+dojo2023-Hoshino@users.noreply.github.com>
2023-07-27 15:05:04 +0900

    外部キー完成分＋user_id統一

--- server/feseek/src/main/java/com/feseek/entity/Area.java
+++ server/feseek/src/main/java/com/feseek/entity/Area.java
@@ -1,10 +1,16 @@
 package com.feseek.entity;
 
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +30,13 @@ public class Area {
 	private Integer id;
 	@NonNull
 	private String area;
+	
+	
+	//外部キー
+	@NonNull
+	@JsonIgnore
+	@OneToMany(mappedBy = "area", cascade = CascadeType.ALL)
+	private List<Event> events;
 }
 
 
