949773c4b115c31854260fbd4b4783eb873a2812
dojo2023-narisawa <130331127+dojo2023-narisawa@users.noreply.github.com>
2023-07-28 10:22:32 +0900

    確認お願いします

--- client/feseek/src/eventDetail/GuestEventDetail.js
+++ client/feseek/src/eventDetail/GuestEventDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import GuestMenubar from '../menubar/GuestMenubar';
 
 export default class GuestEventDetail extends React.Component{
@@ -6,11 +7,36 @@ export default class GuestEventDetail extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            
+            events:[],
+            id:"",
+            usersId:"",
+            eventName:"",
+            startDate:"",
+            endDate:"",
+            openTime:"",
+            address:"",
+            access:"",
+            map:"",
+            costs:"",
+            areasId:"",
+            detail:"",
+            url:"",
+            urlFirst:"",
+            urlSecond:"",
+            urlThird:"",
+            urlForth:"",
+            urlFifth:"",
+            contactAddress:"",
         }
     }
 
+    componentDidMount(){
+        const{id} = this.state;
+        axios.get("/api/event/detail/"+id)
+    }
+
     render(){
+        const{id,usersId,eventName,startDate,endDate,openTime,address,access,map,costs,areasId,detail,organizer,url,urlFirst,urlSecond,urlThird,urlForth,urlFifth,contactAddress,showModal,events} = this.state;
         return(
             <div class="body">
                 <header>
@@ -21,52 +47,52 @@ export default class GuestEventDetail extends React.Component{
                     <img src="　"></img>
                     
                     <p>ここに詳細文をたくさん記載</p>
-
-                    <h2>基本情報</h2>
-                    <table class="list">
-                        <tr>
-                            <th>イベント名</th> <td>あ</td>
-                        </tr>
-                        <tr>
-                            <th>主催者名</th> <td>い</td>
-                        </tr>
-                        <tr>
-                            <th>開催日時</th> <td>う</td>
-                        </tr>
-                        <tr>
-                            <th>住所</th> <td>え</td>
-                        </tr>
-                        <tr>
-                            <th>マップURL</th> <td>お</td>
-                        </tr>
-                        <tr>
-                            <th>アクセス</th> <td>か</td>
-                        </tr>
-                        <tr>
-                            <th>参加費</th> <td>き</td>
-                        </tr>
-                        <tr>
-                            <th>HP　URL</th> <td>く</td>
-                        </tr>
-                        <tr>
-                            <th>連絡先</th> <td>け</td>
-                        </tr>
-                    </table>
-
-                    <div class="side">
-                        <div class="review">
-                        <h3>口コミ</h3>
-                            <br />
-                            <iframe src="./review"></iframe>
-                        </div>
-
-                        <div class="timeline">
-                        <h3>タイムライン</h3>
-                            <br />
-                            <iframe src="./Timeline"></iframe>
-                        </div>
-                    </div>
                 </body>
+                <h2>基本情報</h2>
+                <table class="list">
+                    <tr>
+                        <th>イベント名</th> <td>{eventName}</td>
+                    </tr>
+                    <tr>
+                        <th>主催者名</th> <td>い</td>
+                    </tr>
+                    <tr>
+                        <th>開催日時</th> <td>う</td>
+                    </tr>
+                    <tr>
+                        <th>住所</th> <td>え</td>
+                    </tr>
+                    <tr>
+                        <th>マップURL</th> <td>お</td>
+                    </tr>
+                    <tr>
+                        <th>アクセス</th> <td>か</td>
+                    </tr>
+                    <tr>
+                        <th>参加費</th> <td>き</td>
+                    </tr>
+                    <tr>
+                        <th>HP　URL</th> <td>く</td>
+                    </tr>
+                    <tr>
+                        <th>連絡先</th> <td>け</td>
+                    </tr>
+                </table>
+
+                <div class="side">
+                    <div class="review">
+                    <h3>口コミ</h3>
+                        <br />
+                        <iframe src="./review"></iframe>
+                    </div>
+
+                    <div class="timeline">
+                    <h3>タイムライン</h3>
+                        <br />
+                        <iframe src="./Timeline"></iframe>
+                    </div>
+                </div>
+
 
             </div>
         );
--- client/feseek/src/myReviewList/MyEventList.js
+++ client/feseek/src/myReviewList/MyEventList.js
@@ -3,65 +3,60 @@ import axios from "axios";
 import SponsorMenubar from "../menubar/SponsorMenubar";
 import './MyEventList.css';
 
-/* 主催者画面　投稿したイベントの一覧表示 */
+/* 主催者画面　投稿したイベントの一覧表示とイベント投稿処理 */
 export default class MyEventList extends React.Component{
     constructor(props){
         super(props);
         //stateの設定
         this.state = {
             events:[],
-            id:"idです",
-            users_id:"users_idです",    //自分のイベントだけ取得するため
-            event_name:"event_nameです",
-            janres_id:"janres_idです",
-            start_date:"start_dateです",
-            end_date:"end_dateです",
-            open_time:"open_timeです",
-            address:"addressです",
-            access:"accessです",
-            map:"mapです",
-            costs:"constsです",
-            areas_id:"area_idです",
-            detail:"detailです",
-            organizer:"organizerです",
-            url:"urlです",
-            url_first:"url_firstです",
-            url_second:"url_secondです",
-            url_third:"url_thirdです",
-            url_forth:"url_forthです",
-            url_fifth:"url_fifthです",
-            contact_address:"contact_addressです",
+            //idをBootからもらってくる必要がある(apiのURLに必要)
+            id:"",
+            usersId:"",
+            eventName:"",
+            startDate:"",
+            endDate:"",
+            openTime:"",
+            address:"",
+            access:"",
+            map:"",
+            costs:"",
+            areasId:"",
+            detail:"",
+            url:"",
+            urlFirst:"",
+            urlSecond:"",
+            urlThird:"",
+            urlForth:"",
+            urlFifth:"",
+            contactAddress:"",
             showModal:false,
-            // events:[],
-            // id:"",
-            // users_id:"",
-            // event_name:"",
-            // start_date:"",
-            // end_date:"",
-            // open_time:"",
-            // address:"",
-            // access:"",
-            // map:"",
-            // costs:"",
-            // areas_id:"",
-            // detail:"",
-            // url:"",
-            // url_first:"",
-            // url_second:"",
-            // url_third:"",
-            // url_forth:"",
-            // url_fifth:"",
-            // contact_address:"",
-            // showModal:false,
         }
     }
 
-    //マウント後に動作する
     componentDidMount(){
-        // axios.get("/api/EventList")
+            // const {id,users_id,event_name,janres_id,start_date,end_date,open_time,address,access,map,costs,areas_id,detail,organizer,url,url_first,url_second,url_third,url_forth,url_fifth,contact_address} = this.state;
+            // const data = {id,users_id,event_name,janres_id,start_date,end_date,open_time,address,access,map,costs,areas_id,detail,organizer,url,url_first,url_second,url_third,url_forth,url_fifth,contact_address};
+        // const usersId = 'aaa';
+        // console.log(usersId)
+        // const {usersId} = this.state;
+        axios.get("/api/event/sponser")
+        .then( json =>{
+            console.log(json.data);
+            this.setState({
+                events:json.data
+            })
+        });
     }
 
-    //投稿ボタンでモーダル呼び出し
+    onInput = (e) =>{
+        //onChangeイベントでテキストボックスに入力された値を、stateに格納する処理
+        this.setState ({
+            [e.target.name]:e.target.value
+        });
+    }
+
+    //投稿フォームモーダル呼び出し
     postEvent = () => {
         this.toggleModal();
     }
@@ -74,105 +69,125 @@ export default class MyEventList extends React.Component{
         });
     }
 
+    //投稿ボタンクリック時の処理
+    eventPost = () =>{
+        console.log("eventPost");
+        const {usersId,eventName,startDate,endDate,openTime,address,access,map,costs,areasId,detail,organizer,url,urlFirst,urlSecond,urlThird,urlForth,urlFifth,contactAddress,showModal,events} = this.state;
+        const data = {eventName:eventName,startDate:startDate,endDate:endDate,openTime:openTime,address:address,access:access,map:map,costs:costs,areasId:areasId,detail:detail,organizer:organizer,url:url,urlFirst:urlFirst,urlSecond:urlSecond,urlThird:urlThird,urlForth:urlForth,urlFifth:urlFifth,contactAddress:contactAddress};
+        axios.post('/api/events/add',data)
+        // .then(json =>{
+        //     console.log(json);
+        // })
+    }
+
     render(){
-        const{id,users_id,event_name,janres_id,start_date,end_date,open_time,address,access,map,costs,areas_id,detail,organizer,url,url_first,url_second,url_third,url_forth,url_fifth,contact_address,showModal} = this.state;
+        // const{id,users_id,event_name,janres_id,start_date,end_date,open_time,address,access,map,costs,areas_id,detail,organizer,url,url_first,url_second,url_third,url_forth,url_fifth,contact_address,showModal} = this.state;
+        const {id,usersId,eventName,startDate,endDate,openTime,address,access,map,costs,areasId,detail,organizer,url,urlFirst,urlSecond,urlThird,urlForth,urlFifth,contactAddress,showModal,events} = this.state;
         return(
             <div>
                 <header>
-                    <SponsorMenubar></SponsorMenubar>   {/*上部メニュー*/}
+                {/*上部メニュー*/}
+                    <SponsorMenubar></SponsorMenubar>
                 </header>
                 <body>
-                    {/* イベント一覧表示部分 */}
-                    <div class="eventList">
-                        <h1>投稿したイベント一覧</h1>
-                    {/*投稿したイベントの個数文回す */}
-                    <table class="list">
-                        <tr>
-                                <th>イベント名</th>
-                                <td>{event_name}</td>
-                                <th>ジャンル</th>
-                                <td>{janres_id}</td>
-                            </tr>
-                            <tr>
-                                <th>主催期間</th>
-                                <td>{start_date} ～ {end_date}</td>
-                                <th>開催時間</th>
-                                <td>{open_time}</td>
-                            </tr>
-                            <tr>
-                                <th>都道府県</th>
-                                <td>{areas_id}</td>
-                                <th>住所</th>
-                                <td>{address}</td>
-                            </tr>
-                            <tr>
-                                <th>マップURL</th>
-                                <td>{map}</td>
-                                <th>アクセス</th>
-                                <td>{access}</td>
-                            </tr>
-                            <tr>
-                                <th>参加費</th>
-                                <td>{costs}</td>
-                                <th>ホームページURL</th>
-                                <td>{url}</td>
-                            </tr>
-                            <tr>
-                                <th>主催者</th>
-                                <td>{organizer}</td>
-                                <th>連絡先</th>
-                                <td>{contact_address}</td>
-                            </tr>
-                            <tr>
-                                <th>詳細</th>
-                                <td>{detail}</td>
-                            </tr>
-                            <tr>
-                                <th>写真</th>
-                                <td>{url_first}</td>
-                                <td>{url_second}</td>
-                                <td>{url_third}</td>
-                                <td>{url_forth}</td>
-                                <td>{url_fifth}</td>
-                            </tr>
-                        </table>
-
-                        {/*投稿モーダルを開くボタン*/}
-                        <button className="default_button" onClick={() =>{this.postEvent()}}>イベント投稿</button>
-                    </div>
+                {/* イベント一覧表示部分 */}
+                    <div className="eventList">
+                        <h3>投稿したイベント一覧ページ(主催者画面)</h3>
+                        {/*投稿したイベントの個数分回す */}
+                        {events.map((event,index) =>
+                            <div>
+                                <table className="list">
+                                    <tr>
+                                            <th>イベント名</th>
+                                            <td>{event.eventName}</td>
+                                            <th>ジャンル</th>
+                                            <td>{event.janresId}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>主催期間</th>
+                                            <td>{event.startDate} ～ {event.endDate}</td>
+                                            <th>開催時間</th>
+                                            <td>{event.openTime}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>都道府県</th>
+                                            <td>{event.areasId}</td>
+                                            <th>住所</th>
+                                            <td>{event.address}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>マップURL</th>
+                                            <td>{event.map}</td>
+                                            <th>アクセス</th>
+                                            <td>{event.access}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>参加費</th>
+                                            <td>{event.costs}</td>
+                                            <th>ホームページURL</th>
+                                            <td>{event.url}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>主催者</th>
+                                            <td>{event.organizer}</td>
+                                            <th>連絡先</th>
+                                            <td>{event.contactAddress}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>詳細</th>
+                                            <td>{event.detail}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>写真</th>
+                                            <td>{event.urlFirst}</td>
+                                            <td>{event.urlSecond}</td>
+                                            <td>{event.urlThird}</td>
+                                            <td>{event.urlForth}</td>
+                                            <td>{event.urlFifth}</td>
+                                        </tr>
+                                    </table>
+                                    <button>編集</button>
+                                    <button>削除</button>
+                            </div>
+                        )} 
+{/* ------------------------------------------------------------------------------------------------------------------------- */}
+                    {/*投稿モーダルを開くボタン*/}
+                    <button onClick={() =>{this.postEvent()}}>イベント投稿(モーダル)</button>
+                </div>
                 </body>
                 {/* イベント投稿モーダル */}
+                {/* <form action="" method="POST"> */}
                 {showModal &&
                     <div id="overlay">
                         <div id="content">
                             <table>
                                 <tr>
                                     <th>イベント名</th>
-                                    <td><input type="text" placeholder="※必須"></input></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={eventName} name="eventName"></input></td>
                                 </tr>
                                 <tr>
                                     <th>ジャンル</th>
                                     <td>
-                                        <input type="checkbox" value="1" />
+                                        <input type="checkbox" value="1" onChange={this.onInput} />
                                         <label>食べる</label>
-                                        <input type="checkbox" value="2" />
+                                        <input type="checkbox" value="2" onChange={this.onInput} />
                                         <label>見る</label>
-                                        <input type="checkbox" value="3" />
+                                        <input type="checkbox" value="3" onChange={this.onInput} />
                                         <label>体験する</label>
-                                        <input type="checkbox" value="4" />
+                                        <input type="checkbox" value="4" onChange={this.onInput} />
                                         <label>その他</label>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>開催期間</th>
                                     <td>
-                                        開始日<input type="date"></input>
-                                        終了日<input type="date"></input>
+                                        開始日<input type="date" onChange={this.onInput} value={startDate} name="startDate"></input>
+                                        終了日<input type="date" onChange={this.onInput} value={endDate} name="endDate"></input>
                                     </td>
                                 </tr>
                                 <tr>
                                     <th>開催時間</th>
-                                    <td><input type="text" placeholder="※必須"></input></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={openTime} name="openTime"></input></td>
                                 </tr>
                                 <tr>
                                     <th>都道府県</th>
@@ -231,46 +246,51 @@ export default class MyEventList extends React.Component{
                                 </tr>
                                 <tr>
                                     <th>住所</th>
-                                    <td><td><input type="text" placeholder="※必須"></input></td></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={address} name="address"></input></td>
                                 </tr>
                                 <tr>
                                     <th>マップURL</th>
-                                    <td><td><input type="text" placeholder="※必須"></input></td></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={map} name="map"></input></td>
                                 </tr>
                                 <tr>
                                     <th>アクセス</th>
-                                    <td><td><input type="text" placeholder="※必須"></input></td></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={access} name="access"></input></td>
                                 </tr>
                                 <tr>
                                     <th>詳細</th>
-                                    <td><td><input type="text" placeholder="※必須"></input></td></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={detail} name="detail"></input></td>
                                 </tr>
                                 <tr>
                                     <th>参加費</th>
-                                    <td><td><input type="text" placeholder="※必須"></input></td></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={costs} name="costs"></input></td>
                                 </tr>
                                 <tr>
                                     <th>主催者</th>
-                                    <td><td><input type="text" placeholder="※必須"></input></td></td>
+                                    <td><input type="text" placeholder="※必須" onChange={this.onInput} value={organizer} name="organizer"></input></td>
                                 </tr>
                                 <tr>
                                     <th>ホームページURL</th>
-                                    <td><td><input type="text"></input></td></td>
+                                    <td><input type="text" onChange={this.onInput} value={url} name="url"></input></td>
                                 </tr>
                                 <tr>
                                     <th>連絡先</th>
-                                    <td><td><input type="text"></input></td></td>
+                                    <td><input type="text" onChange={this.onInput} value={contactAddress} name="contactAddress"></input></td>
                                 </tr>
                                 <tr>
                                     <th>写真</th>
-                                    <td><input type="file"></input></td>
+                                    <td><input type="file" onChange={this.onInput} value={urlFirst}></input></td>
+                                    <td><input type="file" onChange={this.onInput} value={urlSecond}></input></td>
+                                    <td><input type="file" onChange={this.onInput} value={urlThird}></input></td>
+                                    <td><input type="file" onChange={this.onInput} value={urlForth}></input></td>
+                                    <td><input type="file" onChange={this.onInput} value={urlFifth}></input></td>
                                 </tr>
                             </table>
-                            <button>投稿</button>
+                            <button onClick={this.eventPost}>投稿</button>
                             <button onClick={this.toggleModal}>閉じる</button>
                         </div>
                     </div>
                 }
+                {/* </form> */}
             </div>   
         );
     }
